52e8065d8b5a71ed45837208ddbcd6bf25283ec7
danial.asaria <dasaria@usc.edu>
2022-10-19 10:59:47 -0700

    Implemented the Restaurant class

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/Restaurant.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/Restaurant.java
@@ -1,4 +1,36 @@
 package com.example.villageplanner_teaminfiniteloop;
 
+import android.location.Location;
+
 public class Restaurant {
+    String id;
+    String name;
+    Location resCoordinate;
+
+    public Restaurant(String resId, String name, Location coordinate)
+    {
+        this.id = resId;
+        this.name = name;
+        this.resCoordinate = coordinate;
+    }
+
+    public void findDirectiontoStore(Location userLocation, Location resLocation)
+    {
+
+    }
+
+    public String getId()
+    {
+        return this.id;
+    }
+
+    public String getResName()
+    {
+        return this.name;
+    }
+
+    public Location getCoordinates()
+    {
+        return this.resCoordinate;
+    }
 }
