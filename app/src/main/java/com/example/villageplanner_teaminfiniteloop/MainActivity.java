b8ee8e4826ecdcd5cbf7fa49f8f5fc55ab974ede
Jack Stark <jackstark2099@outlook.com>
2022-10-22 12:20:51 -0700

    Realtime DB fix

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DataSnapshot;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase root;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
+        root = FirebaseDatabase.getInstance();
+        reference = root.getReference("message");
+        reference.setValue("May the Force be with you");
     }
 }
