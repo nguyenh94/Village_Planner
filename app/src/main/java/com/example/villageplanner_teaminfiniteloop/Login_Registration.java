594f5f6dd3c32505264bd006666c5eae166656f4
danial.asaria <dasaria@usc.edu>
2022-10-25 11:06:40 -0700

    firestore implementation vs realtime database

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/Login_Registration.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/Login_Registration.java
@@ -1,92 +1,105 @@
-package com.example.villageplanner_teaminfiniteloop;
-import android.media.Image;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-public class Login_Registration {
-    // https://www.geeksforgeeks.org/how-to-save-data-to-the-firebase-realtime-database-in-android/
-    static public void Register(String email, String password, String name, Image photo) throws Exception{
-        // implement random id generator or want to use email as user id?
-        String uniqueId = UUID.randomUUID().toString();
-        boolean validReg = checkValidRegistration(email);
-        if (validReg) {
-            User user = new User(uniqueId, email, name, photo, password, null);
-            FirebaseDatabase root = FirebaseDatabase.getInstance();
-            DatabaseReference reference = root.getReference("users");
-            password = hashPassword(password);
-
-            reference.child(user.getId()).child("name").setValue(name);
-            reference.child(user.getId()).child("email").setValue(email);
-            reference.child(user.getId()).child("photo").setValue(photo);
-            reference.child(user.getId()).child("password").setValue(password);
-        }
-    }
-
-    static public void Login(String email, String password) throws Exception{
-
-    }
-
-    static private String hashPassword(String rawValue){
-        try
-        {
-            // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            // Add password bytes to digest
-            md.update(rawValue.getBytes());
-            // Get the hash's bytes
-            byte[] bytes = md.digest();
-
-            // This bytes[] has bytes in decimal format. Convert it to hexadecimal format
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < bytes.length; i++) {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-
-            // Get complete hashed password in hex format
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return "";  //Error
-    }
-
-    // Get the email and talk to the firebase to see email has been already registered
-    static private boolean checkValidRegistration(String email) {
-        final boolean[] emailValid = {true};
-        FirebaseDatabase root = FirebaseDatabase.getInstance();
-        DatabaseReference reference = root.getReference("users");
-        reference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                if (snapshot.child("users").child("email").exists()) {
-                    System.out.println("Email already exists.");
-                    emailValid[0] = false;
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        return emailValid[0];
-    }
-
-    // Get the email and password and talk to the firebase to see email and password are matched
-    static private boolean checkValidLogin(String email, String password){
-        //use hash function here to hash the password raw value
-        return true;
-    }
-}
+//package com.example.villageplanner_teaminfiniteloop;
+//import android.media.Image;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import java.security.MessageDigest;
+//import java.security.NoSuchAlgorithmException;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.UUID;
+//
+//public class Login_Registration {
+//    // https://www.geeksforgeeks.org/how-to-save-data-to-the-firebase-realtime-database-in-android/
+//    static public void Register(String email, String password, String name, Image photo) throws Exception{
+//        // implement random id generator or want to use email as user id?
+//        String uniqueId = UUID.randomUUID().toString();
+//        boolean validReg = checkValidRegistration(email);
+//        if (validReg) {
+//            User user = new User(uniqueId, email, name, photo, password, null);
+//            FirebaseDatabase root = FirebaseDatabase.getInstance();
+//            DatabaseReference reference = root.getReference("users");
+//            password = hashPassword(password);
+//
+//            reference.child(user.getId()).child("name").setValue(name);
+//            reference.child(user.getId()).child("email").setValue(email);
+//            reference.child(user.getId()).child("photo").setValue(photo);
+//            reference.child(user.getId()).child("password").setValue(password);
+//        }
+//    }
+//
+//    static public void Login(String email, String password) throws Exception{
+//
+//    }
+//
+//    static private String hashPassword(String rawValue){
+//        try
+//        {
+//            // Create MessageDigest instance for MD5
+//            MessageDigest md = MessageDigest.getInstance("MD5");
+//            // Add password bytes to digest
+//            md.update(rawValue.getBytes());
+//            // Get the hash's bytes
+//            byte[] bytes = md.digest();
+//
+//            // This bytes[] has bytes in decimal format. Convert it to hexadecimal format
+//            StringBuilder sb = new StringBuilder();
+//            for (int i = 0; i < bytes.length; i++) {
+//                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+//            }
+//
+//            // Get complete hashed password in hex format
+//            return sb.toString();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//        return "";  //Error
+//    }
+//
+//    // Get the email and talk to the firebase to see email has been already registered
+////    static private boolean checkValidRegistration(String email) {
+////        final boolean[] emailValid = {true};
+////        FirebaseDatabase root = FirebaseDatabase.getInstance();
+////        DatabaseReference reference = root.getReference("users");
+////        reference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+////            @Override
+////            public void onDataChange(DataSnapshot snapshot) {
+////                if (snapshot.child("users").child("email").exists()) {
+////                    System.out.println("Email already exists.");
+////                    emailValid[0] = false;
+////                }
+////            }
+////           }
+////                    return false;
+////        }
+////            root.child("users").child(email).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>()
+////            @Override
+////            public void onComplete(@NonNull Task<DataSnapshot> task) {
+////                if (!task.isSuccessful()) {
+////                    Log.e("firebase", "Error getting data", task.getException());
+////                }
+////                else {
+////                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+////                }
+////             }
+////            });
+////            @Override
+////            public void onCancelled(@NonNull DatabaseError error) {
+////
+////            }
+////        });
+////        return emailValid[0];
+//
+//    // Get the email and password and talk to the firebase to see email and password are matched
+//    static private boolean checkValidLogin(String email, String password){
+//        //use hash function here to hash the password raw value
+//        return true;
+//    }
+//}
--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.villageplanner_teaminfiniteloop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseDatabase root;
@@ -19,15 +32,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Write a message to the database
-        root = FirebaseDatabase.getInstance();
-        reference = root.getReference("message");
-        reference.setValue("Works on macbook");
+//        root = FirebaseDatabase.getInstance();
+//        reference = root.getReference("message");
+//        reference.setValue("Works on macbook");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        Login_Registration registration = new Login_Registration();
-        try {
-            registration.Register("test@gmail.com", "12345", "Huong", null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        CollectionReference users = db.collection("users");
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+        users.document("Ada").set(user);
+
+        DocumentReference docRef = db.collection("users").document("Ada");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("getting user", "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d("getting user", "No such document");
+                    }
+                } else {
+                    Log.d("getting user", "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
+//        Login_Registration registration = new Login_Registration();
+//        try {
+//            registration.Register("danialatest@gmail.com", "12345", "Huong", null);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
     }
 }
