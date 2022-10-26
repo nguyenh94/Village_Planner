e2dfc7d0a6570047785c69fdfcbd23fe461e371f
nguyenh94 <huongbng@usc.edu>
2022-10-25 22:55:25 -0700

    Deleted non-used stuff

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/Login_Registration.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/Login_Registration.java
@@ -17,25 +17,7 @@ import java.util.UUID;
 public class Login_Registration {
     // https://www.geeksforgeeks.org/how-to-save-data-to-the-firebase-realtime-database-in-android/
     static public void Register(String email, String password, String name, Image photo) throws Exception{
-        // implement random id generator or want to use email as user id?
-        String uniqueId = UUID.randomUUID().toString();
-        checkValidRegistration(email);
-//        TODO SETUP FIRESTORE AUTH and incorporate user object
-//        if (validReg) {
-//            User user = new User(uniqueId, email, name, photo, password, null);
-//            FirebaseFirestore db = FirebaseFirestore.getInstance();
-//
-//            CollectionReference users = db.collection("users");
-//
-//            Map<String, Object> userInfo = new HashMap<>();
-//            userInfo.put("uniqueId", uniqueId);
-//            userInfo.put("name", name);
-//            userInfo.put("email", email);
-//            userInfo.put("password", hashPassword(password));
-//            userInfo.put("photo", photo);
-//
-//            users.document(email).set(userInfo);
-//        }
+
     }
 
     static public void Login(String email, String password) throws Exception{
@@ -64,64 +46,5 @@ public class Login_Registration {
             e.printStackTrace();
         }
         return "";  //Error
-    }
-
-    public static boolean afterLogin(boolean result) {
-        // deal with false
-        System.out.println(result);
-        return result;
-    }
-    // Get the email and talk to the firebase to see email has been already registered
-    static private void checkValidRegistration(String email) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("users").document(email);
-        final Object[] userData = new Object[1];
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                System.out.println("line 80");
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d("getting user", "DocumentSnapshot data: " + document.getData());
-                        userData[0] = document.getData();
-                    } else {
-                        Log.d("getting user", "No such document");
-                    }
-                } else {
-                    Log.d("getting user", "get failed with ", task.getException());
-                }
-            }
-        });
-    }
-
-    // Get the email and password and talk to the firebase to see email and password are matched
-    static private boolean checkValidLogin(String email, String password){
-        //use hash function here to hash the password raw value
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        DocumentReference docRef = db.collection("users").document(email);
-        final Object[] userData = new Object[1];
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d("getting user", "DocumentSnapshot data: " + document.getData());
-                        userData[0] = document.getData();
-                    } else {
-                        Log.d("getting user", "No such document");
-                    }
-                } else {
-                    Log.d("getting user", "get failed with ", task.getException());
-                }
-            }
-        });
-        //TODO UNHASH PASSOWRD TO COMPARE
-        if(userData[0] == password)
-        {
-            return true;
-        }
-        return false;
     }
 }
--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/MainActivity.java
@@ -14,11 +14,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -28,8 +23,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseDatabase root;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
