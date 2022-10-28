6d4d9b7c5c099fc32c581455ae8e428d09f53a58
danial.asaria <dasaria@usc.edu>
2022-10-28 10:09:10 -0700

    trying to figure out reminders

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/Reminder.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/Reminder.java
@@ -1,8 +1,25 @@
 package com.example.villageplanner_teaminfiniteloop;
+import static android.provider.Settings.System.getString;
 
+import android.app.AlarmManager ;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent ;
+import android.content.Intent ;
+import android.os.Build;
+import android.os.Bundle ;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View ;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import java.util.Calendar ;
 import java.sql.Time;
 
 public class Reminder {
+    //TODO
+    private static final String CHANNEL_ID = "";
     private String title;
     private Time reminderTime;
     private Time travelTime;
@@ -19,10 +36,29 @@ public class Reminder {
     {
 
     }
-    public void registerNotification(String title, Time reminderTime)
-    {
-
-    }
+//    private void createNotificationChannel() {
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            CharSequence name = getString(R.string.channel_name);
+//            String description = getString(R.string.channel_description);
+//            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+//            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+//            channel.setDescription(description);
+//            // Register the channel with the system; you can't change the importance
+//            // or other notification behaviors after this
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//        }
+//    }
+//    public void registerNotification(String title, Time reminderTime)
+//    {
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                .setSmallIcon(R.drawable.notification_icon)
+//                .setContentTitle("Notification")
+//                .setContentText("context text")
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//    }
 
     public String getResId()
     {
