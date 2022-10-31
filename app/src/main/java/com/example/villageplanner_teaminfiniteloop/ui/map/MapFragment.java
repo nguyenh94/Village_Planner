33c7ca83a7164f3d0a69a5fd8ea7aaec3791ead5
Jack Stark <jackstark2099@outlook.com>
2022-10-30 21:03:51 -0700

    Refactor fragment views and binding

--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/ui/map/MapFragment.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/ui/map/MapFragment.java
@@ -1,4 +1,4 @@
-package com.example.villageplanner_teaminfiniteloop.ui.home;
+package com.example.villageplanner_teaminfiniteloop.ui.map;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,18 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.villageplanner_teaminfiniteloop.databinding.FragmentHomeBinding;
+import com.example.villageplanner_teaminfiniteloop.databinding.FragmentMapBinding;
 
-public class HomeFragment extends Fragment {
+public class MapFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentMapBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        MapViewModel homeViewModel =
+                new ViewModelProvider(this).get(MapViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textHome;
--- app/src/main/java/com/example/villageplanner_teaminfiniteloop/ui/me/MeViewModel.java
+++ app/src/main/java/com/example/villageplanner_teaminfiniteloop/ui/me/MeViewModel.java
@@ -1,16 +1,16 @@
-package com.example.villageplanner_teaminfiniteloop.ui.home;
+package com.example.villageplanner_teaminfiniteloop.ui.me;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class MeViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public MeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
